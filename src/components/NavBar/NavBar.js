61a841903e2a9ee32d8bd4a25060cc91af69b7e4
EstebanSDeveloper <104537237+EstebanSDeveloper@users.noreply.github.com>
2022-10-13 18:55:25 -0500

    PreEntrega1+Sarmiento-Pardo-

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,28 +1,29 @@
 import CategoryButton from "./CategoryButton/CategoryButton"
 import './NavBar.css';
+import logo from '../../assets/imagenes/logo.png';
 
 function NavBar() {
   return (
     <div className="main-nav">
       <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <a class="navbar-brand text-white" href="#">The Sugar Show</a>
+    <img className='logo-nav' src={logo} alt="logo" />
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon navbar-dark"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link text-white" aria-current="page" href="#">Home</a>
+            <a class="nav-link text-white" className="glow" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">Cereal Flavors</a>
+            <a class="nav-link text-white" className="glow" href="#">Cereal Flavors</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">Bars</a>
+            <a class="nav-link text-white" className="glow" href="#">Bars</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">Bundles</a>
+            <a class="nav-link text-white" className="glow" href="#">Bundles</a>
           </li>
         </ul>
         <span class="navbar-text">
